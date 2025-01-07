8ed13d42de749528e68122be82b75524f186516b
Arnold Rispress <arnold.rispress@gmail.com>
2025-01-07 16:52:32 -0500

    making the app look prettier

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,23 @@
-import Navbar from '@/components/Navbar';
-import '@/assets/styles/globals.css';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "@/assets/styles/globals.css";
 
 export const metadata = {
-    title: 'Property Pulse',
-    keywords: 'rental, property, real estate',
-    description: 'Find the perfect rental property',
-}
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
 
 const MainLayout = ({ children }) => {
-    return ( <html>
-        <body>
-            <Navbar />
-            <main>
-                { children }
-            </main>
-        </body>
-    </html> );
-}
- 
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+
 export default MainLayout;
